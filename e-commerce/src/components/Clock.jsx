df425ffac860943aa8b6ee791bf62832716877d8
Brayan Gutierrez <brayangutierrezndev@gmail.com>
2024-10-10 19:14:27 -0500

    Se arregla el componente Clock

--- e-commerce/src/components/Clock.jsx
+++ e-commerce/src/components/Clock.jsx
@@ -6,25 +6,30 @@ function Clock() {
   const [active, setActive] = useState(true);
 
   useEffect(() => {
+    // Si el contador no está activo o ya ha llegado a 0, detenemos todo
     if (!active || timeRemaining <= 0) {
-      setTimeRemaining(0); 
+      setTimeRemaining(0);
       return;
     }
 
+    // Actualizamos cada segundo
     const interval = setInterval(() => {
       const newTimeRemaining = targetDate - Date.now();
       if (newTimeRemaining <= 0) {
         setActive(false);
-        setTimeRemaining(0); 
+        setTimeRemaining(0);
       } else {
         setTimeRemaining(newTimeRemaining);
       }
     }, 1000);
 
+    // Limpiar el intervalo al desmontar el componente
     return () => {
       clearInterval(interval);
     };
-  }, [targetDate, active, timeRemaining]);
+  }, [active, timeRemaining]);
+
+  // Función para formatear el tiempo
   const formatTime = (milisegundos) => {
     const totalSeconds = Math.floor(milisegundos / 1000);
     const segundos = totalSeconds % 60;
@@ -38,30 +43,30 @@ function Clock() {
   const { dias, horas, minutos, segundos } = formatTime(timeRemaining);
 
   return (
-    <div className=" w-3/5">
+    <div className="w-3/5">
       <ul className="flex gap-8">
-        <li className="flex items-center gap-8 ">
+        <li className="flex items-center gap-8">
           <div>
             <p className="font-medium text-sm">Days</p>
             <p className="font-bold text-3xl">{dias}</p>
           </div>
           <span className="text-2xl text-red-500 font-bold">:</span>
         </li>
-        <li className="flex items-center gap-8 ">
+        <li className="flex items-center gap-8">
           <div>
             <p className="font-medium text-sm">Hours</p>
             <p className="font-bold text-3xl">{horas}</p>
           </div>
           <span className="text-2xl text-red-500 font-bold">:</span>
         </li>
-        <li className="flex items-center gap-8 ">
+        <li className="flex items-center gap-8">
           <div>
             <p className="font-medium text-sm">Minutes</p>
             <p className="font-bold text-3xl">{minutos}</p>
           </div>
           <span className="text-2xl text-red-500 font-bold">:</span>
         </li>
-        <li className="flex items-center gap-8 ">
+        <li className="flex items-center gap-8">
           <div>
             <p className="font-medium text-sm">Seconds</p>
             <p className="font-bold text-3xl">{segundos}</p>
