d741462f2c7687583d1a4731cf49374664bbe43e
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-10-16 01:44:47 -0500

    Se arreglan unas funciones del carrito

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -3,40 +3,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import Carrito from "./pages/Carrito";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
 import Login from "./pages/Login";
 import About from "./pages/About";
+import Contact from "./pages/Contact"
 import { ProductProvider } from "./context/ProductTotalPrice";
-=======
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-// import Navbar from "./components/Navbar"
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import MainPage from "./pages/MainPage";
-// import Carrito from "./pages/Carrito";
->>>>>>> d589a6a7e3e4f9a3e93832636f35ea61971c0286
+
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-<<<<<<< HEAD
       <ProductProvider>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/carrito" element={<Carrito />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </ProductProvider>
-=======
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/carrito" element={<Carrito />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
->>>>>>> d589a6a7e3e4f9a3e93832636f35ea61971c0286
+      </ProductProvider>
       <Footer />
     </BrowserRouter>
   );
--- e-commerce/src/main.jsx
+++ e-commerce/src/main.jsx
@@ -3,16 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-<<<<<<< HEAD
-createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <App />
-  // </StrictMode>
-)
-=======
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
->>>>>>> d589a6a7e3e4f9a3e93832636f35ea61971c0286
