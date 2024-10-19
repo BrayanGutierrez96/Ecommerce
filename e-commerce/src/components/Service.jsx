bbadff56e9a74441ee450f42415db4f93d3b42fc
Rock6982 <rodrigoqm75@gmail.com>
2024-10-18 22:02:50 -0400

    responsive update Service

--- e-commerce/src/components/Service.jsx
+++ e-commerce/src/components/Service.jsx
@@ -2,47 +2,45 @@ function Service() {
   return (
     <>
       <div className="my-11 flex justify-center">
-        <div className="flex flex-row gap-32">
-          <div className="flex flex-col items-center content-center gap-1">
-            <div className="m-3 bg-black w-[70px] h-[70px] rounded-full flex justify-center items-center border-[11px] border-neutral-300">
-              <img
-                className="h-[70%] w-[70%]"
-                src="../../public/icon-delivery.png"
-                alt=""
-              />
+        <div className="flex flex-row gap-1 sm:gap-10 md:gap-2 lg:gap-24 xl:gap-32 2xl:gap-32">
+          <div className="flex w-28 sm:w-40 md:w-60 lg:w-60 xl:w-60 2xl:w-60 flex-col items-center content-center gap-1">
+            <div className="m-3 bg-black w-[60px] h-[60px] md:w-[70px] md:h-[70px] lg:w-[70px] lg:h-[70px] xl:w-[70px] xl:h-[70px] 2xl:w-[70px] 2xl:h-[70px] rounded-full flex justify-center items-center border-[11px] border-neutral-300">
+              <img className="h-[70%] w-[70%]" src="icon-delivery.png" alt="" />
             </div>
-            <div>
-              <strong className="text-lg">FREE AND FAST DELIVERY</strong>
+            <div className="text-center">
+              <strong className="text-xs sm:text-sm md:text-sm lg:text-base xl:text-lg 2xl:text-lg">
+                FREE AND FAST DELIVERY
+              </strong>
             </div>
-            <div className="text-sm">
+            <div className="text-xs text-center md:text-sm lg:text-sm xl:text-sm 2xl:text-sm">
               Free delivery for all orders over $140
             </div>
           </div>
-          <div className="flex flex-col items-center justify-center gap-1">
-            <div className="m-3 bg-black w-[70px] h-[70px] rounded-full flex justify-center items-center border-[11px] border-neutral-300">
-              <img
-                className="h-[70%] w-[70%]"
-                src="../../public/icon-service.png"
-                alt=""
-              />
+          <div className="flex w-28 sm:w-40 md:w-60 lg:w-60 xl:w-60 2xl:w-60 flex-col items-center content-center gap-1">
+            <div className="m-3 bg-black w-[60px] h-[60px] md:w-[70px] md:h-[70px] lg:w-[70px] lg:h-[70px] xl:w-[70px] xl:h-[70px] 2xl:w-[70px] 2xl:h-[70px] rounded-full flex justify-center items-center border-[11px] border-neutral-300">
+              <img className="h-[70%] w-[70%]" src="icon-service.png" alt="" />
             </div>
-            <div>
-              <strong className="text-lg">24/7 CUSTOMER SERVICE</strong>
+            <div className="text-center">
+              <strong className="text-xs sm:text-sm md:text-sm lg:text-base xl:text-lg 2xl:text-lg">
+                24/7 CUSTOMER SERVICE
+              </strong>
             </div>
-            <div className="text-sm">Friendly 24/7 customer support</div>
+            <div className="text-xs text-center md:text-sm lg:text-sm xl:text-sm 2xl:text-sm">
+              Friendly 24/7 customer support
+            </div>
           </div>
-          <div className="flex flex-col items-center justify-center gap-1">
-            <div className="m-3 bg-black w-[70px] h-[70px] rounded-full flex justify-center items-center border-[11px] border-neutral-300">
-              <img
-                className="h-[70%] w-[70%]"
-                src="../../public/icon-secure .png"
-                alt=""
-              />
+          <div className="flex w-28 sm:w-40 md:w-60 lg:w-60 xl:w-60 2xl:w-60 flex-col items-center content-center gap-1">
+            <div className="m-3 bg-black w-[60px] h-[60px] md:w-[70px] md:h-[70px] lg:w-[70px] lg:h-[70px] xl:w-[70px] xl:h-[70px] 2xl:w-[70px] 2xl:h-[70px] rounded-full flex justify-center items-center border-[11px] border-neutral-300">
+              <img className="h-[70%] w-[70%]" src="icon-secure .png" alt="" />
             </div>
-            <div>
-              <strong className="text-lg">MONEY BACK GUARANTEE</strong>
+            <div className="text-center">
+              <strong className="text-xs sm:text-sm md:text-sm lg:text-base xl:text-lg 2xl:text-lg">
+                MONEY BACK GUARANTEE
+              </strong>
             </div>
-            <div className="text-sm">We reurn money within 30 days</div>
+            <div className="text-xs text-center md:text-sm lg:text-sm xl:text-sm 2xl:text-sm">
+              We reurn money within 30 days
+            </div>
           </div>
         </div>
       </div>
