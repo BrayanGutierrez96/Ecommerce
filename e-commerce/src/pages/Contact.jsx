d224ad60d20851453c55aa36f1379ec248df5dbf
Rock6982 <rodrigoqm75@gmail.com>
2024-10-18 22:03:32 -0400

    responsive update pages contact

--- e-commerce/src/pages/Contact.jsx
+++ e-commerce/src/pages/Contact.jsx
@@ -5,14 +5,14 @@ import FormContact from "../components/FormContact";
 function Contact() {
   return (
     <>
-      <div className="flex mx-32 my-12 gap-4">
+      <div className="flex mx-8 my-8 gap-4 flex flex-row md:mx-14 lg:mx-20 xl:mx-28 2xl:mx-32 2xl:my-12 gap-4">
         <Link className="text-gray-400 hover:underline" to="/">
           Home
         </Link>
         <span>/</span>
         <Link className="hover:underline">Contact</Link>
       </div>
-      <div className="flex flex-row gap-5 justify-center">
+      <div className="flex flex-col md:flex-row lg:flex-row 2xl:flex-row gap-5 justify-center items-center">
         <References />
         <FormContact />
       </div>
