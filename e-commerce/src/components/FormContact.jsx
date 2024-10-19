2a6fd80a762b096d13cc7590493f31ebb6676b6a
Rock6982 <rodrigoqm75@gmail.com>
2024-10-18 22:01:20 -0400

    responsive update

--- e-commerce/src/components/FormContact.jsx
+++ e-commerce/src/components/FormContact.jsx
@@ -2,37 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 function FormContact() {
   return (
-    <div className="w-[60%] mx-2 py-2 shadow-[1px_-1px_10px_1px_rgba(0,0,0,0.1)] flex flex-col justify-center gap-1 rounded">
-      <div className="flex flex-row justify-center gap-5 py-8 ">
+    <div className="w-[85%] sm:w-[91%] sm:mx-1 md:w-[61%] md:mx-1 2xl:w-[61%] 2xl:mx-2 py-2 shadow-[1px_-1px_10px_1px_rgba(0,0,0,0.1)] flex flex-col justify-center gap-1 rounded">
+      <div className="flex flex-col items-center md:flex-row lg:flex-row xl:flex-row 2xl:flex-row justify-center md:gap-3 lg:gap-3 gap-5 py-8">
         <input
-          className="bg-[#F5F5F5] p-3 w-72 rounded-md"
+          className="bg-[#F5F5F5] p-3 w-[85%] sm:w-[92%] md:w-[29%] lg:w-[30%] xl:w-[30%] 2xl:w-[30%] rounded-md md:placeholder:text-xs lg:placeholder:text-sm xl:placeholder:text-base"
           type="text"
           placeholder="Your Name *"
         />
         <input
-          className="bg-[#F5F5F5]  p-3 w-72 rounded-md"
+          className="bg-[#F5F5F5] p-3 w-[85%] sm:w-[92%] md:w-[29%] lg:w-[30%] xl:w-[30%] 2xl:w-[30%] rounded-md md:placeholder:text-xs lg:placeholder:text-sm xl:placeholder:text-base"
           type="email"
           placeholder="Your Email *"
         />
         <input
-          className="bg-[#F5F5F5]  p-3 w-72 rounded-md"
+          className="bg-[#F5F5F5] p-3 w-[85%] sm:w-[92%] md:w-[29%] lg:w-[30%] xl:w-[30%] 2xl:w-[30%] rounded-md md:placeholder:text-xs lg:placeholder:text-sm xl:placeholder:text-base"
           type="number"
           placeholder="Your Phone *"
         />
       </div>
       <div className="flex justify-center">
         <textarea
-          className="bg-[#F5F5F5] h-48 w-[91%] p-3 text-left resize-none rounded-md"
+          className="bg-[#F5F5F5] h-48 w-[85%] sm:w-[92%] md:w-[92%] lg:w-[92%] xl:w-[92%] 2xl:w-[92%] p-3 text-left resize-none rounded-md md:placeholder:text-xs lg:placeholder:text-sm xl:placeholder:text-base"
           placeholder="Your Massage"
         ></textarea>
-        {/* <input
-          className="bg-[#F5F5F5] h-32 w-[91%] text-left resize-none"
-          type="text"
-          placeholder="Your Massage"
-        /> */}
       </div>
       <div className="py-8 flex justify-end ">
-        <Link className="bg-red-600 mx-12 py-4 px-20 text-white rounded-lg hover:bg-red-700">
+        <Link className="bg-red-600 mx-7 sm:mx-6 md:mx-12 lg:mx-12 xl:mx-12 2xl:mx-12 py-4 px-20 text-white rounded-lg hover:bg-red-700 text-xs md:text-xs lg:text-sm xl:text-base">
           Send Massage
         </Link>
       </div>
