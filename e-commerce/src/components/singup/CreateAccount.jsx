a0a760933180aa58ed98aa030623297156403296
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-10-21 21:07:15 -0500

    Se hace la funcionalidad del formulario de registro

--- e-commerce/src/components/singup/CreateAccount.jsx
+++ e-commerce/src/components/singup/CreateAccount.jsx
@@ -1,8 +1,33 @@
-
+import { useState } from "react";
 import "../../Styles/login.css";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function CreateAccount() {
+  const [telefono, setTelefono] =  useState(0)
+  const [info, setInfo] = useState({
+    name: "",
+    identificador:"",
+    password: "",
+  });
+
+  const crearCuenta = (event) => {
+    const newInfo = {...info, [event.target.name]:event.target.value}
+    setInfo(newInfo)
+  };
+
+  const enviarDatos = ()=>{
+    const email = info.identificador
+    const ultimaPosicion = email.charAt(email.length - 1);
+    
+    const validacion = isNaN(ultimaPosicion)
+    if(validacion){
+      setTelefono(email);
+    }else{ 
+      setTelefono(parseInt(email));
+    }
+    console.log(telefono);
+  }
+
   return (
     <section id="home">
       <div id="portada">
@@ -12,19 +37,22 @@ function CreateAccount() {
         <form action="" className="formulario">
           <h2 className="titulo-formulario">Create an account</h2>
           <h3 className="texto-formulario">Enter your details below</h3>
-          <input className="entrada-login" type="text" placeholder="Name" />
-
+          <input className="entrada-login" type="text" placeholder="Name" name="name" onChange={(e)=>{crearCuenta(e)}}/>
           <input
             className="entrada-login"
             type="text"
             placeholder="Email or Phone Number"
+            name="identificador"
+            onChange={(e)=>{crearCuenta(e)}}
           />
           <input
             className="entrada-login"
             type="password"
             placeholder="Password"
+            name="password"
+            onChange={(e)=>{crearCuenta(e)}}
           />
-          <button type="button" className="btn-create-account crear-cuenta">
+          <button type="button" className="btn-create-account crear-cuenta" onClick={enviarDatos}>
             Create Account
           </button>
           <button type="button" className="btn-create-account google-cuenta">
