27511e2d7f1afbd2d84c940660fd789167baa6e6
josemorales <jlppmq123@gmail.com>
2024-10-25 22:46:05 -0400

    adicion abaout team imagenes

--- e-commerce/src/pages/About.jsx
+++ e-commerce/src/pages/About.jsx
@@ -1,11 +1,13 @@
 import OurStory from "../components/about/OurStory";
 import Service from "../components/about/Service";
 import Slider from "../components/about/Slider";
+import TeamSection from "../components/about/TeamSection";
 function About() {
   return (
     <>
       <OurStory />
       <Slider />
+      <TeamSection />
       <Service />
     </>
   );
